b53a14f63a68c73ffec8a352e4466b69d25fc635
SkillerRaptor <skillerraptor@protonmail.com>
2024-04-14 11:05:44 +0200

    init

---
+++ esbuild.config.mjs
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2024, SkillerRaptor
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+import builtins from "builtin-modules";
+import console from 'console';
+import esbuild from "esbuild";
+import process from "process";
+
+const prod = (process.argv[2] === "production");
+
+const context = await esbuild.context({
+	entryPoints: ["src/main.ts"],
+	bundle: true,
+	external: [
+		"obsidian",
+		"electron",
+		"@codemirror/autocomplete",
+		"@codemirror/collab",
+		"@codemirror/commands",
+		"@codemirror/language",
+		"@codemirror/lint",
+		"@codemirror/search",
+		"@codemirror/state",
+		"@codemirror/view",
+		"@lezer/common",
+		"@lezer/highlight",
+		"@lezer/lr",
+		...builtins],
+	format: "cjs",
+	target: "es2018",
+	logLevel: "info",
+	sourcemap: prod ? false : "inline",
+	treeShaking: true,
+	outfile: "main.js",
+});
+
+if (prod) {
+	console.log('Building for production');
+	await context.rebuild();
+	process.exit(0);
+} else {
+	await context.watch();
+}
---
+++ version-bump.mjs
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024, SkillerRaptor
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+import { readFileSync, writeFileSync } from "fs";
+
+const targetVersion = process.env.npm_package_version;
+
+let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
+const { minAppVersion } = manifest;
+manifest.version = targetVersion;
+writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+
+let versions = JSON.parse(readFileSync("versions.json", "utf8"));
+versions[targetVersion] = minAppVersion;
+writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
