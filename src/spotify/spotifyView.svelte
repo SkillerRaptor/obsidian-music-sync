0a02d8a8d69c6e86090005919def20ec83c395fb
SkillerRaptor <skillerraptor@protonmail.com>
2024-04-14 23:37:32 +0200

    feat: add volume slider

--- src/spotify/spotifyView.svelte
+++ src/spotify/spotifyView.svelte
@@ -27,7 +27,7 @@
 
   let volume = 100;
 
-  let deviceId = "";
+  let deviceId: string | undefined = undefined;
 
   let shuffleButton: SVGPathElement;
   let playButton: SVGPathElement;
@@ -39,6 +39,7 @@
   let playingSliderChanging = false;
 
   let currentVolume = 0;
+  let volumeIcon: SVGPathElement;
   let volumeSlider: HTMLInputElement;
   let volumeSliderFocused = false;
   let volumeSliderChanging = false;
@@ -73,7 +74,7 @@
 
   async function handlePrevious() {
     if (productType === ProductType.PREMIUM) {
-      await spotifySDK?.player.skipToPrevious(deviceId);
+      await spotifySDK?.player.skipToPrevious(deviceId!);
     } else {
       new Notice("❌ Non premium users can't change playback state", 5000);
     }
@@ -82,10 +83,10 @@
   async function handlePlay() {
     if (productType === ProductType.PREMIUM) {
       if (data.is_playing) {
-        await spotifySDK?.player.pausePlayback(deviceId);
+        await spotifySDK?.player.pausePlayback(deviceId!);
         playButton.setAttribute("d", "M10 24h-6v-24h6v24zm10-24h-6v24h6v-24z");
       } else {
-        await spotifySDK?.player.startResumePlayback(deviceId);
+        await spotifySDK?.player.startResumePlayback(deviceId!);
         playButton.setAttribute("d", "M2 24v-24l20 12-20 12z");
       }
     } else {
@@ -95,7 +96,7 @@
 
   async function handleNext() {
     if (productType === ProductType.PREMIUM) {
-      await spotifySDK?.player.skipToNext(deviceId);
+      await spotifySDK?.player.skipToNext(deviceId!);
     } else {
       new Notice("❌ Non premium users can't change playback state", 5000);
     }
@@ -219,101 +220,118 @@
   setInterval(async () => {
     if (spotifySDK) {
       data = await spotifySDK?.player.getPlaybackState();
-      deviceId = data.device.id!;
+      if (data) {
+        deviceId = data.device.id!;
 
-      // State
-      if (!playingSliderChanging) {
-        playingSlider.max = data.item.duration_ms.toString();
-        playingSlider.value = data.progress_ms.toString();
-        const value = data.progress_ms;
-        currentPlayingProgress = (value / data.item.duration_ms) * 100;
+        // State
+        if (!playingSliderChanging) {
+          playingSlider.max = data.item.duration_ms.toString();
+          playingSlider.value = data.progress_ms.toString();
+          const value = data.progress_ms;
+          currentPlayingProgress = (value / data.item.duration_ms) * 100;
 
-        if (playingSliderFocused) {
-          playingSlider.style.background = `linear-gradient(to right, #1DB954 ${currentPlayingProgress}%, #4d4d4d ${currentPlayingProgress}%)`;
+          if (playingSliderFocused) {
+            playingSlider.style.background = `linear-gradient(to right, #1DB954 ${currentPlayingProgress}%, #4d4d4d ${currentPlayingProgress}%)`;
+          } else {
+            playingSlider.style.background = `linear-gradient(to right, #fff ${currentPlayingProgress}%, #4d4d4d ${currentPlayingProgress}%)`;
+          }
+
+          currentLength = new Date(data.progress_ms)
+            .toISOString()
+            .slice(11, 19)
+            .replace("00:", "");
+          totalLength = new Date(data.item.duration_ms)
+            .toISOString()
+            .slice(11, 19)
+            .replace("00:", "");
         } else {
-          playingSlider.style.background = `linear-gradient(to right, #fff ${currentPlayingProgress}%, #4d4d4d ${currentPlayingProgress}%)`;
+          currentLength = new Date(data.progress_ms)
+            .toISOString()
+            .slice(11, 19)
+            .replace("00:", "");
+          totalLength = new Date(data.item.duration_ms)
+            .toISOString()
+            .slice(11, 19)
+            .replace("00:", "");
         }
 
-        currentLength = new Date(data.progress_ms)
-          .toISOString()
-          .slice(11, 19)
-          .replace("00:", "");
-        totalLength = new Date(data.item.duration_ms)
-          .toISOString()
-          .slice(11, 19)
-          .replace("00:", "");
-      } else {
-        currentLength = new Date(data.progress_ms)
-          .toISOString()
-          .slice(11, 19)
-          .replace("00:", "");
-        totalLength = new Date(data.item.duration_ms)
-          .toISOString()
-          .slice(11, 19)
-          .replace("00:", "");
-      }
+        if (!volumeSliderChanging) {
+          currentVolume = data.device.volume_percent!;
+          volumeSlider.value = currentVolume.toString();
 
-      if (!volumeSliderChanging) {
-        currentVolume = data.device.volume_percent!;
-        volumeSlider.value = currentVolume.toString();
+          if (volumeSliderFocused) {
+            volumeSlider.style.background = `linear-gradient(to right, #1DB954 ${currentVolume}%, #4d4d4d ${currentVolume}%)`;
+          } else {
+            volumeSlider.style.background = `linear-gradient(to right, #fff ${currentVolume}%, #4d4d4d ${currentVolume}%)`;
+          }
 
-        if (volumeSliderFocused) {
-          volumeSlider.style.background = `linear-gradient(to right, #1DB954 ${currentVolume}%, #4d4d4d ${currentVolume}%)`;
-        } else {
-          volumeSlider.style.background = `linear-gradient(to right, #fff ${currentVolume}%, #4d4d4d ${currentVolume}%)`;
+          if (currentVolume == 0) {
+            volumeIcon.setAttribute(
+              "d",
+              "M5 17h-5v-10h5v10zm2-10v10l9 5v-20l-9 5zm15.324 4.993l1.646-1.659-1.324-1.324-1.651 1.67-1.665-1.648-1.316 1.318 1.67 1.657-1.65 1.669 1.318 1.317 1.658-1.672 1.666 1.653 1.324-1.325-1.676-1.656z",
+            );
+          } else {
+            volumeIcon.setAttribute(
+              "d",
+              "M5 17h-5v-10h5v10zm2-10v10l9 5v-20l-9 5zm11.008 2.093c.742.743 1.2 1.77 1.198 2.903-.002 1.133-.462 2.158-1.205 2.9l1.219 1.223c1.057-1.053 1.712-2.511 1.715-4.121.002-1.611-.648-3.068-1.702-4.125l-1.225 1.22zm2.142-2.135c1.288 1.292 2.082 3.073 2.079 5.041s-.804 3.75-2.096 5.039l1.25 1.254c1.612-1.608 2.613-3.834 2.616-6.291.005-2.457-.986-4.681-2.595-6.293l-1.254 1.25z",
+            );
+          }
         }
+
+        if (data.shuffle_state) {
+          shuffleButton.setAttribute("fill", "#1DB954");
+        } else {
+          shuffleButton.setAttribute("fill", "currentColor");
+        }
+
+        if (data.is_playing) {
+          playButton.setAttribute(
+            "d",
+            "M10 24h-6v-24h6v24zm10-24h-6v24h6v-24z",
+          );
+        } else {
+          playButton.setAttribute("d", "M2 24v-24l20 12-20 12z");
+        }
+
+        if (data.repeat_state === "off") {
+          repeatButton.setAttribute("fill", "currentColor");
+          repeatButton.setAttribute(
+            "d",
+            "M2 12c0 .999.381 1.902.989 2.604l-1.098.732-.587.392c-.814-1.025-1.304-2.318-1.304-3.728 0-3.313 2.687-6 6-6h9v-3l6 4-6 4v-3h-9c-2.206 0-4 1.794-4 4zm20.696-3.728l-.587.392-1.098.732c.608.702.989 1.605.989 2.604 0 2.206-1.795 4-4 4h-9v-3l-6 4 6 4v-3h9c3.313 0 6-2.687 6-6 0-1.41-.489-2.703-1.304-3.728z",
+          );
+        } else if (data.repeat_state === "context") {
+          repeatButton.setAttribute("fill", "#1DB954");
+          repeatButton.setAttribute(
+            "d",
+            "M2 12c0 .999.381 1.902.989 2.604l-1.098.732-.587.392c-.814-1.025-1.304-2.318-1.304-3.728 0-3.313 2.687-6 6-6h9v-3l6 4-6 4v-3h-9c-2.206 0-4 1.794-4 4zm20.696-3.728l-.587.392-1.098.732c.608.702.989 1.605.989 2.604 0 2.206-1.795 4-4 4h-9v-3l-6 4 6 4v-3h9c3.313 0 6-2.687 6-6 0-1.41-.489-2.703-1.304-3.728z",
+          );
+        } else if (data.repeat_state === "track") {
+          repeatButton.setAttribute("fill", "#1DB954");
+          repeatButton.setAttribute(
+            "d",
+            "M6.09 18h-.09c-3.313 0-6-2.687-6-6s2.687-6 6-6h2v-3l6 4-6 4v-3h-2c-2.206 0-4 1.794-4 4s1.794 4 4 4h.09c-.055.326-.09.658-.09 1s.035.674.09 1zm11.91-12h-2v2h2c2.206 0 4 1.794 4 4s-1.794 4-4 4h-.09c.055.326.09.658.09 1s-.035.674-.09 1h.09c3.313 0 6-2.687 6-6s-2.687-6-6-6zm-6 7c-2.209 0-4 1.791-4 3.999 0 2.209 1.791 4.001 4 4.001s4-1.792 4-4.001c0-2.208-1.791-3.999-4-3.999zm1.016 6.188h-1.055v-3.109c-.022 0-.884.413-.904.423l-.179-.936 1.241-.574h.896v4.196z",
+          );
+        }
+
+        // Track
+        let item = data.item as Track;
+        if (item.album === undefined) {
+          // TODO: Handle Podcast
+        }
+
+        // Song Information
+        songCover = item.album.images[1].url;
+
+        let artistName = "";
+        for (let i in item.artists) {
+          let artist = item.artists[i];
+          artistName = artistName + artist.name + ", ";
+        }
+
+        title = item.name;
+        artist = artistName.slice(0, -2);
+        album = item.album.name;
       }
-
-      if (data.shuffle_state) {
-        shuffleButton.setAttribute("fill", "#1DB954");
-      } else {
-        shuffleButton.setAttribute("fill", "currentColor");
-      }
-
-      if (data.is_playing) {
-        playButton.setAttribute("d", "M10 24h-6v-24h6v24zm10-24h-6v24h6v-24z");
-      } else {
-        playButton.setAttribute("d", "M2 24v-24l20 12-20 12z");
-      }
-
-      if (data.repeat_state === "off") {
-        repeatButton.setAttribute("fill", "currentColor");
-        repeatButton.setAttribute(
-          "d",
-          "M2 12c0 .999.381 1.902.989 2.604l-1.098.732-.587.392c-.814-1.025-1.304-2.318-1.304-3.728 0-3.313 2.687-6 6-6h9v-3l6 4-6 4v-3h-9c-2.206 0-4 1.794-4 4zm20.696-3.728l-.587.392-1.098.732c.608.702.989 1.605.989 2.604 0 2.206-1.795 4-4 4h-9v-3l-6 4 6 4v-3h9c3.313 0 6-2.687 6-6 0-1.41-.489-2.703-1.304-3.728z",
-        );
-      } else if (data.repeat_state === "context") {
-        repeatButton.setAttribute("fill", "#1DB954");
-        repeatButton.setAttribute(
-          "d",
-          "M2 12c0 .999.381 1.902.989 2.604l-1.098.732-.587.392c-.814-1.025-1.304-2.318-1.304-3.728 0-3.313 2.687-6 6-6h9v-3l6 4-6 4v-3h-9c-2.206 0-4 1.794-4 4zm20.696-3.728l-.587.392-1.098.732c.608.702.989 1.605.989 2.604 0 2.206-1.795 4-4 4h-9v-3l-6 4 6 4v-3h9c3.313 0 6-2.687 6-6 0-1.41-.489-2.703-1.304-3.728z",
-        );
-      } else if (data.repeat_state === "track") {
-        repeatButton.setAttribute("fill", "#1DB954");
-        repeatButton.setAttribute(
-          "d",
-          "M6.09 18h-.09c-3.313 0-6-2.687-6-6s2.687-6 6-6h2v-3l6 4-6 4v-3h-2c-2.206 0-4 1.794-4 4s1.794 4 4 4h.09c-.055.326-.09.658-.09 1s.035.674.09 1zm11.91-12h-2v2h2c2.206 0 4 1.794 4 4s-1.794 4-4 4h-.09c.055.326.09.658.09 1s-.035.674-.09 1h.09c3.313 0 6-2.687 6-6s-2.687-6-6-6zm-6 7c-2.209 0-4 1.791-4 3.999 0 2.209 1.791 4.001 4 4.001s4-1.792 4-4.001c0-2.208-1.791-3.999-4-3.999zm1.016 6.188h-1.055v-3.109c-.022 0-.884.413-.904.423l-.179-.936 1.241-.574h.896v4.196z",
-        );
-      }
-
-      // Track
-      let item = data.item as Track;
-      if (item.album === undefined) {
-        // TODO: Handle Podcast
-      }
-
-      // Song Information
-      songCover = item.album.images[1].url;
-
-      let artistName = "";
-      for (let i in item.artists) {
-        let artist = item.artists[i];
-        artistName = artistName + artist.name + ", ";
-      }
-
-      title = item.name;
-      artist = artistName.slice(0, -2);
-      album = item.album.name;
     }
   }, 500);
 </script>
@@ -428,6 +446,7 @@
             <path
               fill="currentColor"
               d="M5 17h-5v-10h5v10zm2-10v10l9 5v-20l-9 5zm11.008 2.093c.742.743 1.2 1.77 1.198 2.903-.002 1.133-.462 2.158-1.205 2.9l1.219 1.223c1.057-1.053 1.712-2.511 1.715-4.121.002-1.611-.648-3.068-1.702-4.125l-1.225 1.22zm2.142-2.135c1.288 1.292 2.082 3.073 2.079 5.041s-.804 3.75-2.096 5.039l1.25 1.254c1.612-1.608 2.613-3.834 2.616-6.291.005-2.457-.986-4.681-2.595-6.293l-1.254 1.25z"
+              bind:this={volumeIcon}
             />
           </svg>
           <input
